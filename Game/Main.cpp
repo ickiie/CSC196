db333bbe42085c68280c5fa3b10890f3440118b9
ickiie <nicolasries8@gmail.com>
2021-06-29 16:22:08 -0600

    Finihed project 1 (random lines)

--- Game/Main.cpp
+++ Game/Main.cpp
@@ -1,6 +1,27 @@
+#include "core.h"
 #include <iostream>
 
 
-int main() {
-	
+bool Update(float dt)
+{
+	return false;
+}
+
+void Draw(Core::Graphics& graphics)
+{
+	for (int i = 0; i < 100; i += 1) {
+		graphics.SetColor(RGB(rand() % 256, rand() % 256, rand() % 256));
+		graphics.DrawLine(static_cast<float>(rand() % 800), static_cast<float>(rand() % 600), static_cast<float>(rand() % 800), static_cast<float>(rand() % 600));
+	}
+}
+
+int main()
+{
+	char name[] = "CSC196";
+	Core::Init(name, 800, 600);
+	Core::RegisterUpdateFn(Update);
+	Core::RegisterDrawFn(Draw);
+
+	Core::GameLoop();
+	Core::Shutdown();
 }
